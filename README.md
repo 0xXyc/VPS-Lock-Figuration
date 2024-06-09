3a274053a3fcd7127d1099fb1ed2ac24c87429f6
xyconix <xyconix@MacPro-Max.local>
2024-06-08 21:03:31 -0400

    Updated README.md and added lock-figuration.sh initial commit

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# 💻 VPS-Lock-Figuration 🔐
+# 💻 VPS-Lock-Figuration ~ Designed to Configure your VPS with a Locked Tight Configuration 🔐
 ## ✅ Deploy VPS Best Practices in the Click of a few Buttons!! 🤓
 This is a neat little BASH script that should be ran immediately after your VPS boots up the first time. 
 It will help deploy most known best security practices to date. 
 Ultimately, helping you maintain and start off with a secure environment for your VPS!
+
+## 🔥 Usage 🔥
+Be sure to have a secure way to transfer keys to and from the system. In most cases, it is easiest to use your initial root login credentials to create SSH keys for a new user, and then use `ssh-copy-id` to transfer the keys to the new user.
+
+The default port is `1337`, but be sure to change it in the `lock-figuration.sh` file if you want to use a different port.
+
+## Connecting to the VPS
+`ssh -p <port> -i <private_key_here> user@<server_address>`
+
---
+++ lock-figuration.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+# Define color codes
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+# Function to check if a command succeeded
+check_command() {
+  if [ $? -ne 0 ]; then
+    echo -e "${RED}Error: $1 failed. Exiting.${NC}"
+    exit 1
+  fi
+}
+
+# Display banner
+echo -e "${GREEN}
+   :::     :::       :::::::::       ::::::::        :::        ::::::::       ::::::::       :::    :::   
+  :+:     :+:       :+:    :+:     :+:    :+:       :+:       :+:    :+:     :+:    :+:      :+:   :+:                        
+ +:+     +:+       +:+    +:+     +:+              +:+       +:+    +:+     +:+             +:+  +:+                
++#+     +:+       +#++:++#+      +#++:++#++       +#+       +#+    +:+     +#+             +#++:++           
++#+   +#+        +#+                   +#+       +#+       +#+    +#+     +#+             +#+  +#+                           
+#+#+#+#         #+#            #+#    #+#       #+#       #+#    #+#     #+#    #+#      #+#   #+#                         
+ ###           ###             ########        ########## ########       ########       ###    ###                        
+
+   VPS Lock-Figuration ~ By Xyco
+${NC}"
+
+# Update system packages
+echo -e "${YELLOW}Updating system packages...${NC}"
+apt update && apt upgrade -y
+check_command "System update"
+
+# Create a new user
+echo -e "${YELLOW}Creating a new user...${NC}"
+read -p "Enter the new username: " NEW_USER
+adduser $NEW_USER
+check_command "Add user"
+
+# Add new user to sudo group
+usermod -aG sudo $NEW_USER
+check_command "Add user to sudo group"
+
+# Set up UFW firewall
+echo -e "${YELLOW}Setting up UFW firewall...${NC}"
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow 6969/tcp
+ufw enable
+ufw status
+check_command "UFW setup"
+
+# Disable root login and configure SSH
+echo -e "${YELLOW}Configuring SSH...${NC}"
+SSH_CONFIG="/etc/ssh/sshd_config"
+cp $SSH_CONFIG $SSH_CONFIG.bak
+
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/' $SSH_CONFIG
+sed -i "s/#Port 22/Port 13337/" $SSH_CONFIG
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' $SSH_CONFIG
+sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' $SSH_CONFIG
+
+systemctl reload sshd
+check_command "SSH configuration"
+
+# Install and configure Fail2Ban
+echo -e "${YELLOW}Installing and configuring Fail2Ban...${NC}"
+apt install -y fail2ban
+check_command "Fail2Ban installation"
+
+cat <<EOF > /etc/fail2ban/jail.local
+[DEFAULT]
+bantime  = 600m
+findtime  = 10m
+maxretry = 5
+
+[sshd]
+enabled = true
+port = 13337
+logpath = /var/log/auth.log
+EOF
+
+systemctl restart fail2ban
+check_command "Fail2Ban configuration"
+
+# Enable automatic security updates
+echo -e "${YELLOW}Enabling automatic security updates...${NC}"
+apt install -y unattended-upgrades
+check_command "Unattended upgrades installation"
+
+dpkg-reconfigure --priority=low unattended-upgrades
+check_command "Unattended upgrades configuration"
+
+# Warning: SSH keys for the new user
+echo -e "${RED}Be sure to set up SSH keys for the new user...${NC}"
+echo -e "${RED}Via "touch authorized_keys" ...${NC}"
+echo -e "${RED}Now, generate a new SSH key for the new user on your local machine and add the public key to authorized_keys on the VPS...${NC}"
+
+# Display completion message
+echo -e "${GREEN}VPS security setup is complete. Password login is disabled.${NC}"
+echo -e "${GREEN}Remember to log in using: ssh -p 13337 -i <id_rsa> $NEW_USER@<server_ip>${NC}"
